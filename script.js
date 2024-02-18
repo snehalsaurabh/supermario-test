17a752e1727799416701a74488c1275175f7e4c1
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2024-02-19 01:11:41 +0530

    Basic Implementation

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('start-btn').addEventListener('click', startGame);
+
+function startGame() {
+  // ...
+    console.log('Game started');
+    document.getElementById('start-btn').style.display = 'none';
+}
